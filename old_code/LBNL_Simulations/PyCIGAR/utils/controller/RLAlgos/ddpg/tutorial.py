84b716678d8f303a6d05f8b2d0f6e7dc1fca512a
toanngo <toanngosy@gmail.com>
2020-07-30 16:33:05 -0700

    move old_code to archive

--- old_code/LBNL_Simulations/PyCIGAR/utils/controller/RLAlgos/ddpg/tutorial.py
+++
@@ -1,9 +0,0 @@
-from osim.env import L2RunEnv
-
-env = L2RunEnv(visualize=True)
-observation = env.reset()
-
-for i in range(200):
-	observation, reward, done, info = env.step(env.action_space.sample())
-
-	
--- old_code/power/power/utils/exeptions.py
+++
@@ -1,8 +0,0 @@
-"""Power-specific exceptions."""
-
-
-class FatalPowerError(Exception):
-    """Exception class for Flow errors which do not allow for continuation."""
-
-    def __init__(self, msg):
-        Exception.__init__(self, msg)
