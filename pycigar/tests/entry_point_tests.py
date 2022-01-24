869d12212917a36cf3f32eb33e97c0d4bbca2829
toanngo <toanngosy@gmail.com>
2022-01-24 15:17:50 -0800

    add test

--- pycigar/tests/entry_point_tests.py
+++ pycigar/tests/entry_point_tests.py
@@ -11,16 +11,3 @@ runner = unittest.TextTestRunner()
 res = runner.run(suite)
 if not res.wasSuccessful():
     sys.exit(1)
-
-# Test no-agent path
-pycigar.main(
-    pycigar.DATA_DIR + "/ieee37busdata/misc_inputs.csv",
-    pycigar.DATA_DIR + "/ieee37busdata/ieee37.dss",
-    pycigar.DATA_DIR + "/ieee37busdata/load_solar_data.csv",
-    pycigar.DATA_DIR + "/ieee37busdata/breakpoints.csv",
-    2,
-    None,
-    pycigar.LOG_DIR,
-)
-
-# TODO: clean up the API to handle optional arguments.
